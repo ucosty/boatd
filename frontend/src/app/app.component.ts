8737f18a05e19a651219e7aeaeff6ebe14e76de7
Matthew Costa <ucosty@gmail.com>
2020-05-19 09:25:38 +0100

    UI loads values on a timer, backend calculates volume of the fresh and wastewater tanks in litres

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { WaterService } from './services/water.service';
 import { Water } from './models/water';
+import { Observable, timer, Subscribable, Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-root',
@@ -8,9 +9,10 @@ import { Water } from './models/water';
     styleUrls: ['./app.component.scss'],
     providers: [WaterService]
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
     public freshWater: Water;
     public wasteWater: Water;
+    private updateTimer: Subscription;
     
     constructor(private waterService: WaterService) {
     }
@@ -21,6 +23,16 @@ export class AppComponent implements OnInit {
             voltage: 6.7,
             maxVoltage: 6.7
         })
+
+        this.updateTimer = timer(0, 5000)
+            .subscribe(i => this.updateValues())
+    }
+
+    ngOnDestroy() {
+        this.updateTimer.unsubscribe();
+    }
+
+    updateValues() {
         this.waterService.getFreshWater()
             .subscribe(res => this.freshWater = this.updateWaterValue(res));
     }
--- server.py
+++ server.py
@@ -1,21 +1,82 @@
 #!/usr/bin/env python3
 from flask import Flask, jsonify
+import math
 import automationhat
 
 app = Flask(__name__)
 
+class Tank:
+    def __init__(self, length, height, widthTop, widthBottom):
+        self.length = float(length)
+        self.height = float(height)
+        self.widthTop = float(widthTop)
+        self.widthBottom = float(widthBottom)
+
+freshTank = Tank(2500, 620, 1200, 900)
+
+
+# Calculate the volume of liquid for a tank with
+# the diagrammed cross section
+#
+# --------------/ <-- Tank top
+# |      |     /
+# |      |    /
+# |~~~~~~~~~~/ <-- Water Level
+# |      |  /
+# |      |T/ <-- Theta angle
+# |-------/
+def calculateVolume(tank, gaugePercentage):
+    fillHeight = tank.height * gaugePercentage
+
+    # Get the volume of the cuboid
+    cuboidVolume = tank.widthBottom * tank.length * fillHeight
+
+    # Get theta, the angle at the bottom of the tank to the sloped wall
+    triLegnth = tank.widthTop - tank.widthBottom
+    hypotenuse = math.sqrt(math.pow(tank.height, 2) + math.pow(triLegnth, 2))
+    theta = math.acos(tank.height / hypotenuse)
+
+    # Get the length of the water level
+    triWaterLength = math.tan(theta) * fillHeight
+    triangleWaterVolume = 0.5 * fillHeight * triWaterLength * tank.length
+
+    return round((cuboidVolume + triangleWaterVolume) / 1000000.0, 2)
+
+
 @app.route('/v1/water/fresh')
 def freshWater():
+    voltage = automationhat.analog.one.read()
+    maxVoltage = 6.70
+    gaugePercentage = voltage / maxVoltage
+
+    volume = calculateVolume(freshTank, gaugePercentage)
+    capacity = calculateVolume(freshTank, 1)
+    volumePercentage = volume / capacity
+
     return jsonify({
-        'voltage': automationhat.analog.one.read(),
-        'maxVoltage': 6.70
+        'voltage': voltage,
+        'maxVoltage': maxVoltage,
+        'volume': volume,
+        'capacity': capacity,
+        'volumePercentage': round(volumePercentage * 100, 2)
     })
 
 @app.route('/v1/water/waste')
 def wasteWater():
+    voltage = automationhat.analog.two.read()
+    maxVoltage = 6.70
+    gaugePercentage = voltage / maxVoltage
+
+    volume = calculateVolume(freshTank, gaugePercentage)
+    capacity = calculateVolume(freshTank, 1)
+    volumePercentage = volume / capacity
+
     return jsonify({
-        'voltage': automationhat.analog.two.read(),
-        'maxVoltage': 6.70
+        'voltage': voltage,
+        'maxVoltage': maxVoltage,
+        'volume': volume,
+        'capacity': capacity,
+        'volumePercentage': round(volumePercentage * 100, 2)
     })
 
 if __name__ == '__main__':
